f34366f1d54903695f54aaf828778f36df26402a
piaand <pia.m.andersin@gmail.com>
2020-12-31 15:05:22 +0200

    added react configurations to project

---
+++ src/main/js/app.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const axios = require('axios')
+
+const App = () => {
+    return (
+        <div>"REACT is WORKING MAYBE"</div>
+    )
+}
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
