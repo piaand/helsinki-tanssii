2e5279aad9bcc74b419c42c1231cfc86393d9501
piaand <pia.m.andersin@gmail.com>
2020-12-31 17:30:47 +0200

    tested the hook and calling data from endpoint

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,10 +1,32 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const axios = require('axios')
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 const App = () => {
+    const [events, setEvents] = useState([])
+    
+    const hook = () => {
+        axios
+            .get('/api/v1/events')
+            .then(response => {
+                setEvents(response.data)
+                console.log(response.data)
+            })
+    }
+
+    useEffect(hook, [])
+    const names = events.name
+    if (names !== undefined && names !== null) {
+        console.log(names.fi)
+    }
+    
     return (
-        <div>"REACT is WORKING MAYBE"</div>
+        <div>
+            <h3>REACT is WORKING MAYBE nooot</h3>
+            <p>{events.info_url}</p>
+            <p>{events.id}</p>
+            
+        </div>
     )
 }
 
